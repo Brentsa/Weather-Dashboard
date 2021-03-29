88bc5d4f7af9feb0c6c640fe6e9a66e12c3d12ad
Solidshots <seanbrent5@gmail.com>
2021-03-29 19:30:03 -0400

    linked all files

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+//Search for a city
+//Display current and future conditions for a city
+//Add city to search history
+//Current Conditions: name, current weather icon, temp, humidity, wind speed, and UV index
+//When checking the UV change color based on favorable, moderate, or severe conditions
+//Future Conditions: 5 day forcast that shows the date, an icon of the weather conditions, temp, and humidity
+//Open the current and future conditions when clicking on search history
+
+console.log(moment().format("MM-DD-YYYY"));
