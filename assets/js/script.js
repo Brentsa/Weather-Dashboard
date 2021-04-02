58a44a28bb20b8b1fc9103fb72d78064670766d4
Solidshots <seanbrent5@gmail.com>
2021-04-02 12:59:32 -0400

    search history functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ var forecastContainerEl = $("#forecast");
 //Init an array to contain the city search history
 var searchHistory = [];
 
+//Init a switch that tells the search function to save the user search
+var bSaveSearch = true;
+
 //Search current weather for a city
 function searchCityByName(cityName){
 
@@ -35,9 +38,7 @@ function searchCityByLonLat(longitude, latitude, city){
     fetch(apiUrl).then(function(response){
         if(response.ok){
             response.json().then(function(data){
-
-                console.log(data);
-
+                //Display one call weather data based on the logitude and latitude
                 displayWeather(data, city);
             });
         }
@@ -62,6 +63,7 @@ function displayWeather(data, cityName){
 //Display current weather conditions
 function displayCurrentWeather(data, cityName){
 
+    //Initialize all the weather variables
     var city = cityName;
     var date = moment().format("MM/DD/YYYY");
     var temperature = Math.ceil(data.current.temp);
@@ -71,6 +73,10 @@ function displayCurrentWeather(data, cityName){
     var uvi = data.current.uvi;
     console.log(city, date, temperature, humidity, windSpeed, icon, uvi);
 
+    //Add the city to the search history
+    addCityToHistory(city);
+
+    //Dynamically create the html that displays the key weather metrics
     var currentContainter = $("<div>").addClass("row my-3").html("<div class='col-12'><div class='card'><div class='card-body'><h2 class='card-title display-4'></h2></div></div></div>");
     currentContainter.find(".card-title").html(city + " " + date + " <img src='http://openweathermap.org/img/wn/"+ icon +"@4x.png'></img>");
     $("<p>").text("Temperature: " + temperature + " C").appendTo(currentContainter.find(".card-body"));
@@ -86,9 +92,11 @@ function displayCurrentWeather(data, cityName){
 //Display future conditions for a city
 function displayFutureWeather(data){
 
+    //Create a container to hold all of the 5 day forecast cards
     var futureContainter = $("<div>").addClass("row").html("<h2 class='col-12 display-4'>5-Day Forecast</h2>");
 
     for(var day = 1; day < 6; day++){
+        //Initialize all the weather variables
         var fDate = moment.unix(data.daily[day].dt).format("MM/DD/YYYY");
         var fTemp = Math.ceil(data.daily[day].temp.max); 
         var fHumidity = data.daily[day].humidity;
@@ -96,6 +104,7 @@ function displayFutureWeather(data){
         var fIcon = data.daily[day].weather[0].icon;
         console.log(fDate, fTemp, fHumidity, fWindSpeed, fIcon);
 
+        //Dynamically create the html that displays the key weather metrics
         var forecastBox = $("<div>").addClass("col-auto").html("<div class='card bg-primary'><div class='card-body'></div></div>");
 
         $("<h3>").addClass("card-title").text(fDate).appendTo($(forecastBox).find(".card-body"));
@@ -107,24 +116,49 @@ function displayFutureWeather(data){
         forecastBox.appendTo(futureContainter);
     }
 
+    //Add the 5 day forecast elements to the main container
     futureContainter.appendTo(forecastContainerEl);    
 }
 
 //Called when the user clicks the search button
 function handleUserSearch(event){
+
+    //Initialize a variable with the queried city name and then delete from the search box
     var cityName = $("#search-bar").val().trim();
-    searchCityByName(cityName);
     $("#search-bar").val("");
+
+    //Search the city by the name previously queried
+    searchCityByName(cityName);
+}
+
+function handleSearchHistory(event){
+
+    //Acquire the city name to search from the list item
+    var cityName = $(this).text().trim();
+
+    //Flip the save search switch so that it is not saved into the search history again
+    bSaveSearch = false;
+
+    //Search the city by the name previously queried
+    searchCityByName(cityName);
+}
+
+//Add city to search history
+function addCityToHistory(city){
+
+    //If the value was searched save it to the history otherwise do not save it to search history
+    if(bSaveSearch){
+        $("<button>").addClass("list-group-item list-group-item-action").text(city).appendTo($("#search-history"));
+    }
+    
+    //Return the switch to true so that new searches will be saved
+    bSaveSearch = true;
 }
 
 //Take the user city search input and display weather conditions
 $("#search-button").on("click", handleUserSearch);
 
-//Add city to search history
-function addCityToHistory(city){
-
-}
-
 //Open the current and future conditions when clicking on search history
+$("#search-history").on("click", "button", handleSearchHistory);
 
 
